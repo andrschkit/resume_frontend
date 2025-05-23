189a5e6f4da7b5c8bb052cee0e7f9197cc035a5b
andrschkit <37474483+andrschkit@users.noreply.github.com>
2025-05-23 13:09:05 +0300

    [NF]: final variant of navigation

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -3,14 +3,13 @@
     <v-navigation-drawer
       v-model="drawer"
       class="custom-navigation"
-      :class="{ 'collapsed': rail }"
       permanent
       :width="currentWidth"
       @mouseenter="hovered = true"
       @mouseleave="hovered = false"
     >
-      <div class="user-data">
-        <v-avatar class="avatar" :size="rail ? '48' : '80'">
+      <div class="user-data" :style="rail ? 'height:10vh' : 'height:20vh'">
+        <v-avatar class="avatar" :size="rail ? '70' : '120'">
           <img alt="Avatar" src="https://randomuser.me/api/portraits/men/86.jpg">
         </v-avatar>
 
@@ -36,7 +35,7 @@
         >
           <template #prepend>
             <v-icon>
-              <img alt="" class="menu-icon" :src="item.icon">
+              <img alt="" class="menu-icon" :class="!darkMode? 'menu-icon-dark':'menu-icon-light'" :src="item.icon">
             </v-icon>
           </template>
 
@@ -48,8 +47,18 @@
         </v-list-item>
       </v-list>
 
-      <div class="toggle-button" @click="rail = !rail">
+      <div class="toggle-button" @click="toggleRail">
         <v-icon>{{ rail ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
+      </div>
+      <div class="mode-toggle" :class="!rail ? 'mode-toggle-expand' : 'mode-toggle-collapse'">
+        <v-switch
+          v-model="darkMode"
+          :class="!rail ? 'switch-expand' : 'switch-collapse'"
+          hide-details
+          style="padding: 10px; justify-items: center"
+          @change="toggleDarkMode"
+        />
+        <img alt="" class="mode-icon" :src="darkMode?sun:moon">
       </div>
     </v-navigation-drawer>
 
@@ -57,36 +66,68 @@
   </div>
 </template>
 
-<script>
+<script setup>
+  import { computed, onMounted, ref } from 'vue'
+  import { useTheme } from 'vuetify'
   import addressCard from '@/assets/address-card.svg'
   import education from '@/assets/graduation-hat-alt.svg'
   import career from '@/assets/career.svg'
   import portfolio from '@/assets/folder-user.svg'
   import skills from '@/assets/skills.svg'
   import contacts from '@/assets/pen.svg'
+  import moon from '@/assets/moon-svgrepo.svg'
+  import sun from '@/assets/sun-svgrepo.svg'
 
-  export default {
-    data: () => ({
-      drawer: true,
-      rail: false,
-      hovered: false,
-      collapsedWidth: 72,
-      expandedWidth: 240,
-      menu: [
-        { route: 'about', title: 'Обо мне', icon: addressCard },
-        { route: 'education', title: 'Образование', icon: education },
-        { route: 'career', title: 'Карьера', icon: career },
-        { route: 'portfolio', title: 'Портфолио', icon: portfolio },
-        { route: 'skills', title: 'Навыки', icon: skills },
-        { route: 'contacts', title: 'Контакты', icon: contacts },
-      ],
-    }),
-    computed: {
-      currentWidth () {
-        return this.rail ? this.collapsedWidth : this.expandedWidth;
-      },
-    },
+  const theme = useTheme()
+  const drawer = ref(true)
+  const rail = ref(true)
+  const hovered = ref(false)
+  const darkMode = ref(false)
+  const collapsedWidth = 80
+  const expandedWidth = 240
+
+  const menu = [
+    { route: 'about', title: 'Обо мне', icon: addressCard },
+    { route: 'education', title: 'Образование', icon: education },
+    { route: 'career', title: 'Карьера', icon: career },
+    { route: 'portfolio', title: 'Портфолио', icon: portfolio },
+    { route: 'skills', title: 'Навыки', icon: skills },
+    { route: 'contacts', title: 'Контакты', icon: contacts },
+  ]
+
+  const currentWidth = computed(() => {
+    return rail.value ? collapsedWidth : expandedWidth
+  })
+
+  const toggleRail = () => {
+    rail.value = !rail.value
+    localStorage.setItem('navigationRail', rail.value)
   }
+
+  const toggleDarkMode = () => {
+    theme.global.name.value = darkMode.value ? 'customDarkTheme' : 'customLightTheme'
+    localStorage.setItem('theme', theme.global.name.value)
+  }
+
+  onMounted(() => {
+    // Загрузка состояния навигации
+    const savedRail = localStorage.getItem('navigationRail')
+    if (savedRail !== null) {
+      rail.value = savedRail === 'true'
+    }
+
+    // Загрузка темы
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+      darkMode.value = savedTheme === 'customDarkTheme'
+      theme.global.name.value = savedTheme
+    } else {
+      const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+      darkMode.value = isDarkMode
+      theme.global.name.value = isDarkMode ? 'customDarkTheme' : 'customLightTheme'
+      localStorage.setItem('theme', theme.global.name.value)
+    }
+  })
 </script>
 
 <style scoped>
@@ -95,11 +136,12 @@
 }
 
 .custom-navigation {
-  --collapsed-width: 72px;
+  --collapsed-width: 80px;
   --expanded-width: 240px;
   position: fixed;
   height: 100vh;
   transition: width 0.3s cubic-bezier(0.4, 0, 0.2, 1) !important;
+  border-right:1px solid rgba(var(--v-theme-border), 0.3);
   z-index: 100;
 }
 
@@ -111,9 +153,10 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 24px 16px;
+  padding: 15px 16px;
   transition: all 0.3s ease;
   overflow: hidden;
+  height: 17vh;
 }
 
 .avatar {
@@ -131,12 +174,17 @@
 }
 
 .menu-list {
-  padding: 0 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: left;
+  padding: 1px 12px;
 }
 
 .menu-item {
-  border-radius: 8px;
-  margin: 4px 0;
+  width: 100%;
+  border-radius: 10px !important;
+  margin: 10px 0;
   transition: all 0.3s ease;
   min-height: 48px;
   display: flex;
@@ -159,16 +207,41 @@
 
 .active-menu-item {
   background-color: rgba(var(--v-theme-primary), 0.1);
+  border-radius: 50%;
 }
-
-.active-menu-item .menu-icon {
-  filter: brightness(0) saturate(100%) invert(40%) sepia(98%) saturate(7488%) hue-rotate(246deg) brightness(98%) contrast(94%);
+.menu-icon-dark {
+  filter: brightness(0);
+}
+.menu-icon-light {
+  filter: brightness(0) invert(1);
+}
+.active-menu-item .menu-icon-dark {
+  filter:
+    brightness(0)
+    saturate(100%)
+    invert(8%)
+    sepia(100%)
+    saturate(7488%)
+    hue-rotate(246deg)
+    brightness(98%)
+    contrast(94%);
+}
+.active-menu-item .menu-icon-light {
+  filter:
+    brightness(0)
+    saturate(100%)
+    invert(48%)
+    sepia(89%)
+    saturate(1200%)
+    hue-rotate(195deg)
+    brightness(110%)
+    contrast(90%);
 }
 
 .toggle-button {
   position: absolute;
   bottom: 20px;
-  right: 12px;
+  right: 20px;
   cursor: pointer;
   padding: 8px;
   border-radius: 50%;
@@ -187,5 +260,31 @@
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.mode-toggle{
+  transition: all 0.3s ease;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 7vh;
+  padding: 8px;
+}
+.mode-toggle-expand{
+  flex-direction: row;
+}
+.mode-toggle-collapse{
+  flex-direction: column;
+}
+.switch-expand{
+  padding: 10px !important;
+}
+.switch-collapse{
+  padding: 0 !important;
+}
+.mode-icon{
+  width: 28px;
+  height: 28px;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,10 +10,46 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+const customDarkTheme = {
+  dark: true,
+  colors: {
+    background: '#282828',
+    surface: '#282828',
+    'surface-bright': '#373737',
+    hover: '#616161',
+    border: '#8a8a8a',
+    primary: '#0075c9',
+    secondary: '#282828',
+    error: '#f44336',
+    info: '#2196F3',
+    success: '#4caf50',
+    warning: '#fb8c00',
+  },
+};
+const customLightTheme = {
+  dark: true,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    'surface-bright': '#ececec',
+    hover: '#f1f1f1',
+    border: '#959595',
+    primary: '#0075c9',
+    secondary: '#03DAC6',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'customLightTheme',
+    themes: {
+      customDarkTheme,
+      customLightTheme,
+    },
   },
 })
